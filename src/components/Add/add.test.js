c4e1c87091654ca711d669dc2c3eca0aa90025b7
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2024-01-15 23:46:45 +0100

    wip(Add): testing form behaviour

--- src/components/Add/add.test.js
+++ src/components/Add/add.test.js
@@ -1,4 +1,5 @@
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Add from './index';
 
 const mockSync = jest.fn();
@@ -23,12 +24,27 @@ describe('Add component', () => {
         mockAddBook.mockReset();
     });
 
-    test('render', () => {
-        render(<Add />);
+    test('render', async () => {
+        const { findByText, findByPlaceholderText } = render(<Add />);
         expect(mockSync).toBeCalledTimes(0);
         expect(mockSetModal).toBeCalledTimes(0);
         expect(mockAddBook).toBeCalledTimes(0);
+        expect(await findByText('Author')).toBeVisible();
+        expect(await findByText('Title')).toBeVisible();
+        expect(await findByPlaceholderText('Enter author')).toBeVisible();
+        expect(await findByPlaceholderText('Enter title')).toBeVisible();
     });
 
-    
+    test('submit form', () => {
+        const { getByPlaceholderText, getByRole } = render(<Add />);
+        const authorInput = getByPlaceholderText('Enter author');
+        const titleInput = getByPlaceholderText('Enter title');
+        fireEvent.change(authorInput, { target: { value: 'Arthur' } });
+        fireEvent.change(titleInput, { target: { value: 'Sherlock Holmes' } });
+        const button = getByRole('button', {name: 'Add'});
+        button.click();
+        expect(mockAddBook).toBeCalledTimes(1);
+        expect(mockSync).toBeCalledTimes(1);
+        expect(setModal).toBeCalledTimes(1);
+    });
 });
