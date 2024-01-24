f3165eebe16a4a344ce72bf35a3bccb09d51a93f
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2024-01-24 23:51:15 +0100

    test: cover two more components

---
+++ src/components/ListOfBooks/listOfBooks.test.js
@@ -0,0 +1,18 @@
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ListOfBooks from './index';
+
+describe('List of books', () => {
+    describe('render', () => {
+        // test('empty', async () => {
+        //     const { findByText, findByPlaceholderText, getByRole } = render(<ListOfBooks books={[]}/>);
+        // });
+
+        // test('there are books', async () => {
+        //     const { findByText, findByPlaceholderText, getByRole } = render(<ListOfBooks />);
+        //     expect(await findByText('Valor y al toro')).toBeVisible();
+        //     expect(await findByText('Author: Francisco Ibañez')).toBeVisible();
+        //     expect(await findByText('📘')).toBeVisible();
+        // });
+    });
+});
