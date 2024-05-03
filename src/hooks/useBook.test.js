7209d5c7addf3c713bcff79e616a43fdce9ad22a
Francisco Moreno <fmoreno@aligntech.com>
2024-05-03 18:02:06 +0200

    test(useBook): test remove function

--- src/hooks/useBook.test.js
+++ src/hooks/useBook.test.js
@@ -16,7 +16,7 @@ describe('useBooks', () => {
     test('Init hook', () => {
         const wrapper = ({ children }) => <BooksContextProvider>{children}</BooksContextProvider>
         const { result } = renderHook(() => useBooks(), { wrapper });
-        const { loading, books, setRead, addBook, populateBooks, loadBooks } = result.current;
+        const { loading, books, setRead, addBook, populateBooks, loadBooks, removeBook } = result.current;
         expect(loading).toBe(false);
         expect(Array.isArray(books)).toBeTruthy();
         expect(books.length).toBe(0);
@@ -24,6 +24,7 @@ describe('useBooks', () => {
         expect(typeof addBook).toBe('function');
         expect(typeof populateBooks).toBe('function');
         expect(typeof loadBooks).toBe('function');
+        expect(typeof removeBook).toBe('function');
     });
 
     describe('getAll', () => {
@@ -103,6 +104,30 @@ describe('useBooks', () => {
             await waitFor(() => result.current.setRead(fakeBook.id));
             expect(booksService.update).toHaveBeenCalledTimes(1);
             expect(result.current.books[0].isRead).toBeTruthy();
+        });
+    });
+
+    describe('removeBook', () => {
+        test('Book is removed', async () => {
+            booksService.getAll.mockResolvedValueOnce([fakeBook]);
+            booksService.remove.mockResolvedValueOnce(true);
+            const wrapper = ({ children }) => <BooksContextProvider>{children}</BooksContextProvider>
+            const { result } = renderHook(() => useBooks(), { wrapper });
+            await waitFor(() => result.current.loadBooks());
+            expect(result.current.books.length).toBe(1);
+            await waitFor(() => result.current.removeBook(fakeBook));
+            expect(result.current.books.length).toBe(0);
+        });
+
+        test('Removing book fails', async () => {
+            booksService.getAll.mockResolvedValueOnce([fakeBook]);
+            booksService.remove.mockRejectedValueOnce(new Error('Oppps something went wrong!'));
+            const wrapper = ({ children }) => <BooksContextProvider>{children}</BooksContextProvider>
+            const { result } = renderHook(() => useBooks(), { wrapper });
+            await waitFor(() => result.current.loadBooks());
+            expect(result.current.books.length).toBe(1);
+            await waitFor(() => result.current.removeBook(fakeBook));
+            expect(result.current.books.length).toBe(0);
         });
     });
 });
